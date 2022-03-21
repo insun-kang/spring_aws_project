6b18924d50ed16a4c6473090ef768e7488b94c8c
insun <sunink1@naver.com>
2022-03-21 17:09:52 +0900

    google oauth추가

--- src/main/java/com/insun/pjt/springboot/web/IndexController.java
+++ src/main/java/com/insun/pjt/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.insun.pjt.springboot.web;
 
+import javax.servlet.http.HttpSession;
 import ch.qos.logback.classic.sift.AppenderFactoryUsingJoran;
+import com.insun.pjt.springboot.config.auth.dto.SessionUser;
 import com.insun.pjt.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -12,10 +14,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute(("user"));
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
